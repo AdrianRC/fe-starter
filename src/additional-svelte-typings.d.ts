3f9c3a313acd675d8d99f3e6a95b9baeb7a9ab2e
AdrianRC <adrn@hey.com>
2025-02-19 16:41:28 -0400

    update deps

--- src/additional-svelte-typings.d.ts
+++
@@ -1,6 +0,0 @@
-// See: https://github.com/sveltejs/language-tools/blob/master/docs/preprocessors/typescript.md#im-using-an-attributeevent-on-a-dom-element-and-it-throws-a-type-error
-declare namespace svelteHTML {
-	interface HTMLAttributes {
-		fetchpriority?: string;
-	}
-}
